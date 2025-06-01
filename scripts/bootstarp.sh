02c0eeeab8b70ea13883c0b94ad366a3333e9cf6
vasudevshetty <vasudeepu2815@gmail.com>
2025-06-01 16:15:43 +0530

    Remove cleanup commands from bootstarp.sh to simplify setup process

--- scripts/bootstarp.sh
+++ scripts/bootstarp.sh
@@ -11,8 +11,4 @@ echo "Reloading bashrc..."
 source ~/.bashrc
 
 echo "Done! Your dev environment is set up."
-
-cd
-rm -rf ~/devbox
-echo "Cleaning up..."
 echo "You can now start using your dev environment with the new configurations."
